e43fa261116d653fd0525d249786459d842b8f33
DevLucianoPrandi <dev.lucianoprandi@gmail.com>
2023-04-13 06:57:05 -0300

    sube base

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Barra from "./components/navbar/navbar";
+import Footer from "./components/footer/footer";
+import Home from "./components/home/home";
+import Servicios from "./components/servicios/servicios";
+import Idiomas from './components/idiomas/idiomas';
+import Niveles from "./components/niveles/niveles";
+import Contacto from "./components/contacto/contacto";
+import Reserva from "./components/reserva/reserva";
 
-function App() {
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import {Route, Routes, BrowserRouter} from "react-router-dom";
+
+
+
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div className="main">
+<BrowserRouter>
+<Barra />
+<Routes>
+<Route path="/" element={<Home />} />
+<Route index element={<Home />} />
+<Route path="servicios" element= {<Servicios />} />
+<Route path="idiomas" element= {<Idiomas />} />
+<Route path="niveles" element= {<Niveles />} />
+<Route path="contacto" element= {<Contacto />} />
+<Route path="reserva" element= {<Reserva />} />
+</Routes>
+<Footer />
 
+</BrowserRouter>
+    </div>
+    )
+}
 export default App;
